2e48af2131cc44b138e156bc7b59f5e75d84fcc8
josh <josh@joshs-MacBook-Air.local>
2016-07-25 20:46:37 -0600

    added a link to my nav directive for jquery support

--- projects/spa/js/dir.js
+++ projects/spa/js/dir.js
@@ -1,8 +1,55 @@
 angular.module("app").directive("menu", function(){
+	function linkFunc(scope, elem, attrs){
+		$("#options").click(function(){
+			$(".optionDisp").slideDown('fast');
+			$(".contact").slideUp('fast');
+			$(".info").slideUp('fast');
+		});
+		$("#contact").click(function(){
+			$(".contact").slideDown('fast');
+			$(".optionDisp").slideUp('fast');
+			$(".info").slideUp('fast');
+		});
+		$("#info").click(function(){
+			$(".info").slideDown('fast');
+			$(".optionDisp").slideUp('fast');
+			$(".contact").slideUp('fast');
+		});
+		$("#mainNav").mouseleave(function(){
+			$(".optionDisp").slideUp('fast');
+			$(".contact").slideUp('fast');
+			$(".info").slideUp('fast');
+		});
+		$("#rainBtn").click(function(){
+			$(this).addClass('active');
+			$("#whiteBtn").removeClass('active');
+			$("#pinkBtn").removeClass('active');
+			$("#white").slideUp(10);
+			$("#pink").slideUp(10);
+			$("#rain").slideDown(200);
+		});
+		$("#whiteBtn").click(function(){
+			$(this).addClass('active');
+			$("#rainBtn").removeClass('active');
+			$("#pinkBtn").removeClass('active');
+			$("#rain").slideUp(10);
+			$("#pink").slideUp(10);
+			$("#white").slideDown(300);
+		});
+		$("#pinkBtn").click(function(){
+			$(this).addClass('active');
+			$("#rainBtn").removeClass('active');
+			$("#whiteBtn").removeClass('active');
+			$("#rain").slideUp(10);
+			$("#white").slideUp(10);
+			$("#pink").slideDown(300);
+		});
+		$("#nCage").click(function(){
+			$(".player > *").pause();
+		});
+	}
 	return{
 		templateUrl: "../views/menu.html",
-		// link{
-		//
-		// }
+		link: linkFunc
 	};
 });
